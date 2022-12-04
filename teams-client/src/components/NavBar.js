c9d83ab65497be9850ed92e90d59249f85219fba
Filipe Santos <filipesantosdev@gmail.com>
2022-12-04 18:24:07 +0000

    Navbar changes

--- teams-client/src/components/NavBar.js
+++ teams-client/src/components/NavBar.js
@@ -5,26 +5,33 @@ import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
-import EditIcon from '@mui/icons-material/Edit';
 import Divider from '@mui/material/Divider';
-import ArchiveIcon from '@mui/icons-material/Archive';
-import FileCopyIcon from '@mui/icons-material/FileCopy';
-import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import { styled, alpha } from '@mui/material/styles';
 
-const pages = ['Home'];
-const pagesPath = ['/home'];
-const pagesWithMenus = ['Our Projects'];
-const ourProjects = ['REST API'];
-const ourProjectsPath = ['/restAPI'];
+// const pages = ['About'];
+const pages = [
+  {
+  name: 'About',
+  path: '/',
+  menu: []
+  },
+  {
+    name: 'Our Projects',
+    path: '/projects',
+    menu: [
+      {
+        name: 'REST API',
+        path: '/projects/rest-api',
+      }
+    ]
+  }
+];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 const StyledMenu = styled((props) => (
@@ -91,6 +98,7 @@ function NavBar() {
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
   const handleClick = (event) => {
+    alert("mouse over")
     setAnchorEl(event.currentTarget);
   };
   const handleClose = () => {
@@ -122,49 +130,47 @@ function NavBar() {
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {pages.map((page, idx) => (
-              <Button
-                key={page}
-                onClick={()=>{handleCloseNavMenu(pagesPath[idx])}}
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              >
-                {page}
-              </Button>
+            {pages.map((page) => (
+              <>
+                <Button
+                  {...page.menu.length > 0 && 
+                    {
+                      "id":               "demo-customized-button",
+                      "aria-controls":    { ...open ? "demo-customized-menu" : undefined },
+                      "aria-haspopup":    "true",
+                      "aria-expanded":    { ...open ? 'true' : undefined},
+                      "disableElevation": true
+                    }
+                  }
+                  key={page.name}
+                  onMouseOver={page.menu.length > 0 ? handleClick : null}
+                  onClick={()=>{handleCloseNavMenu(page.path)}}
+                  sx={{ my: 2, color: 'white', display: 'block' }}
+                >
+                  {page.name}
+                </Button>
+                {page.menu.length > 0 && (
+                    <StyledMenu
+                      id="demo-customized-menu"
+                      MenuListProps={{
+                        'aria-labelledby': 'demo-customized-button',
+                      }}
+                      anchorEl={anchorEl}
+                      open={open}
+                      onClose={handleClose}
+                    >
+                      {page.menu.map((item, idx) => (
+                        <>
+                          {idx > 0 && <Divider />}
+                          <MenuItem onClick={handleClose} disableRipple>
+                            {item.name}
+                          </MenuItem>
+                        </>
+                      ))}
+                  </StyledMenu>
+                )}
+              </>
             ))}
-            <div>
-
-            <Button
-              id="demo-customized-button"
-              aria-controls={open ? 'demo-customized-menu' : undefined}
-              aria-haspopup="true"
-              aria-expanded={open ? 'true' : undefined}
-              disableElevation
-              onClick={handleClick}
-              sx={{ my: 2, color: 'white', display: 'block' }}
-            >
-              Our Projects
-            </Button>
-            <StyledMenu
-              id="demo-customized-menu"
-              MenuListProps={{
-                'aria-labelledby': 'demo-customized-button',
-              }}
-              anchorEl={anchorEl}
-              open={open}
-              onClose={handleClose}
-            >
-              <MenuItem onClick={handleClose} disableRipple>
-                Edit
-              </MenuItem>
-              <MenuItem onClick={handleClose} disableRipple>
-                Duplicate
-              </MenuItem>
-              <Divider sx={{ my: 0.5 }} />
-              <div class="marginForMenu">
-                <b>Here you can check several personal projects developed by Team Santos</b>
-              </div>
-            </StyledMenu>
-          </div>
           </Box>
 
           
