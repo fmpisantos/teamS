c48f7bb7452c457ce4288cd4ce5089f9298f8495
bvsantos <bvsantosdev@gmail.com>
2023-04-18 21:20:52 +0100

    new easyfood

--- Freelance Jobs/DBApp/src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * @author bruno
- *
- */
-module DBApp {
-}
---
+++ easyTalk/server/Service/src/test/java/com/easyfood/Service/EasyFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easyfood.Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
